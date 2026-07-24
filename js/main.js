eed6329607720bf38aa3300b23196a64253cc237
itsmidris <idrishiimran72@gmail.com>
2026-07-24 22:06:58 +0530

    Portfolio

---
+++ js/main.js
@@ -0,0 +1,17 @@
+lucide.createIcons();
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 50){
+
+        navbar.classList.add("scrolled");
+
+    }else{
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
